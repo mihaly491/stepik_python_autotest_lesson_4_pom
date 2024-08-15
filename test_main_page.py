e9379b24c1ecce1faffdfe9c691259f5d747ccb1
mihaly491 <mihal812ag@gmail.com>
2024-08-15 11:09:09 +0300

    Added transfers between pages

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,11 @@
 from .Pages.main_page import MainPage
+from .Pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
