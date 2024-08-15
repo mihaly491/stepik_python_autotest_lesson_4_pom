18893db7be8907250cfe3ac343d91a5f17eb1bc2
mihaly491 <mihal812ag@gmail.com>
2024-08-15 11:02:37 +0300

    Added locators and tests to login page

--- Pages/locators.py
+++ Pages/locators.py
@@ -2,3 +2,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form_invalid")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#registoer_form_invalid")
