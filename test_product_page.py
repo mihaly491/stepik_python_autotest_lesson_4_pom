cc8fbf0a95ff2f5215d6c8286cb0bf484947e5ff
mihaly491 <mihal812ag@gmail.com>
2024-08-16 19:22:55 +0300

    Added next tests for product page

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ import pytest
 from .Pages.main_page import MainPage
 from .Pages.product_page import ProductPage
 
-LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
 
 list_of_failed_num = [7]
 tested_links = [f"{LINK}?promo=offer{i}" if i not in list_of_failed_num else
@@ -11,8 +11,7 @@ tested_links = [f"{LINK}?promo=offer{i}" if i not in list_of_failed_num else
                              )
                 for i in range(10)]
 
-
-@pytest.mark.parametrize("link", tested_links)
+'''@pytest.mark.parametrize("link", tested_links)
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
@@ -21,5 +20,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_success_message()
     page.product_name_in_success_alert_should_match_added_product_name()
     page.should_be_basket_cost()
-    page.basket_cost_should_match_product_price()
+    page.basket_cost_should_match_product_price()'''
 
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.add_product_to_basket()
+    page.success_message_should_disappear()
